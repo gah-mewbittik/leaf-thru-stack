6e20f4aa78b2ec40063fa9dd044fee5dd3a13921
Andrew Biron <andrewbiron@andrews-air.home>
2024-04-13 10:23:12 -0400

    completed SavedBook file changes

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -16,37 +16,12 @@ import { GET_ME } from '../utils/queries';
 import {REMOVE_BOOK} from '../utils/mutations';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+const {loading, error, data} = useQuery(GET_ME)
 
-  const [getMe, {error}] = useMutation(GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   //create function that accepts the book's mongo _id value as param and deletes the book from the database
  
@@ -58,25 +33,30 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const {data: updatedData } = await deleteBook({
+        variables: {bookId},
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      if(updatedData && updatedData.removeBook){
+        // upon success, remove book's id from localStorage
+        removeBookId(bookId);
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
+  if(error){
+    console.error(error);
+    return <h2>Error while fetching data</h2>;
+  }
+
+  const userData = data.me;
 
   return (
     <>
